b072f35ba58fac2aecfbac992449c96d5e09401c
Mohd Sameer <mohdsameer6819@gmail.com>
2025-02-18 15:01:49 +0530

    data validation pipeline is completed

--- main.py
+++ main.py
@@ -7,6 +7,7 @@ from sensor.components import data_ingestion
 from sensor.entity import artifact_entity
 from sensor import utils
 from sensor.components.data_ingestion import DataIngestion
+from sensor.components.data_validation import DataValidation
 
 
 def test_logger_and_exception():
@@ -25,7 +26,15 @@ if __name__ == "__main__":
         data_ingestion_config= config_entity.DataIngestionConfig(training_pipeline_config=training_pipeline_config)
         print(data_ingestion_config.to_dict())
         data_ingestion= DataIngestion(data_ingestion_config=data_ingestion_config)
-        print(data_ingestion.initiate_data_ingestion())
+        data_ingestion_artifact=data_ingestion.initiate_data_ingestion()
+        
+        
+        data_validation_config = config_entity.DataValidationConfig(training_pipeline_config=training_pipeline_config)
+        data_validation = DataValidation(data_validation_config=data_validation_config,data_ingestion_artifact=data_ingestion_artifact)
+        
+        data_validation_artifact = data_validation.initiate_data_validation()
+        
+        
     except Exception as e:
         print(e)
     
